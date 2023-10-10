64e0d7de4dc23a3f2bab2b51cb227250254e2368
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-10 18:55:49 +0200

    Fix - path images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //- creare un array
 //        - creare una variabile
 //        - assegnare alla variabile un valore tra [] in cui inserire le cinque immagini
-const imgs = ["../img/01.webp", "../img/02.webp", "../img/03.webp", "../img/04.webp", "../img/05.webp"];
+const imgs = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
 console.log(imgs);
     //- creare una costante che mi dica quale img è attiva
 const activeImg = 0;
@@ -12,10 +12,16 @@ console.log(imgs);
 for (let i = 0; i < imgs.length; i++) {
     const imgsPath = imgs[i];
     console.log(imgsPath);
+    //  - template literal
     const htmlString = `<div> img src="${imgsPath}"</div>`;
-    
     console.log(htmlString);
     containerDomElement.innerHTML += htmlString 
 }
+
+const imageDomElement = document.getElementsByClassName('imgsPath');
+console.log(imageDomElement);
+
+const secondImageDomElement = imageDomElement[1];
+secondImageDomElement.classList.add('active');
 
 
