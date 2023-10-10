f4932dc08d9c4b46878461217ba0259c619faba6
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-10 17:09:10 +0200

    Add - collegamento al DOM

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //        - assegnare alla variabile un valore tra [] in cui inserire le cinque immagini
 const imgs = ["../img/01.webp", "../img/02.webp", "../img/03.webp", "../img/04.webp", "../img/05.webp"];
 console.log(imgs);
-//- creare una costante che mi dica quale img è attiva
+    //- creare una costante che mi dica quale img è attiva
 const activeImg = 0;
-
+//- creare collegamento al DOM
+const containerDomElement = document.querySelector('.container');
