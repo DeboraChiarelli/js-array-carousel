0e09574532b6b5cc750044384e97af139b24009e
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-11 16:07:28 +0200

    Add - click / app.js

--- app.js
+++ app.js
@@ -13,15 +13,25 @@ for (let i = 0; i < imgs.length; i++) {
     const imgsPath = imgs[i];
     console.log(imgsPath);
     //  - template literal
-    const htmlString = `<div> img src="${imgsPath}"</div>`;
+    const htmlString = `
+    <div class="image">
+        <img src="${imgsPath}">
+    </div>`;
     console.log(htmlString);
-    containerDomElement.innerHTML += htmlString 
+    containerDomElement.innerHTML += htmlString;
 }
 
-const imageDomElement = document.getElementsByClassName('imgsPath');
+const imageDomElement = document.getElementsByClassName('slide-img');
 console.log(imageDomElement);
 
-const secondImageDomElement = imageDomElement[1];
+const secondImageDomElement = imageDomElement[0];
 secondImageDomElement.classList.add('active');
+
+
+const iconUpDomElement = document.querySelector('.icon-up');
+iconUpDomElement.addEventListener('click');
+
+const iconDownDomElement = document.querySelector('.icon-down');
+iconDownDomElement.addEventListener('click');
 
 
